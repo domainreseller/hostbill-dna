a44f59cee7f44c18c06f674d7dcc01d3f0b421fe
xa81 <poisonkid@gmail.com>
2026-06-02 02:11:06 +0300

    Bump version to 2.0.4

--- includes/modules/Domain/domainnameapi/class.domainnameapi.php
+++ includes/modules/Domain/domainnameapi/class.domainnameapi.php
@@ -17,7 +17,7 @@ require_once __DIR__.'/lib/dna.php';
 
 class domainnameapi extends DomainModule implements  DomainModuleContacts, DomainModuleListing, DomainLookupInterface, DomainSuggestionsInterface, DomainPremiumInterface, DomainPriceImport, DomainModuleLock, DomainModuleNameservers, DomainModuleGluerecords, DomainModuleAuth{
 
-    protected $version     = '2.0.3';
+    protected $version     = '2.0.4';
     protected $modname     = "Domain Name Api";
     protected $description = 'ICANN Accredited Domain Registrar With 900+ TLDs';
 
